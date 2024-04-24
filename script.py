6b1e74bdf348677a1231707ba4622c14795e0b26
ParsuramBaral <parsurambaral42@gmail.com>
2024-04-24 23:15:20 +0545

    Script Update Version-3

--- script.py
+++ script.py
@@ -2,10 +2,12 @@ import random
 def file_read() :
     file_name = input("File Name :")
     file = open(file_name, "r")
-    
     for i in file:
         file_name_list = i.strip().split(',')
         for j in file_name_list:
             y = str(random.randint(1,9))+str(random.randint(1,9))+str(random.randint(1,9))+str(random.randint(1,9))
             print(j.strip()+str(y)+"@test.com")
+            file = open(file_name, "w")
+            file.write(j.strip()+str(y)+"@test.com")
+        
 file_read()
